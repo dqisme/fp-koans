6b86d4ab01d99882cf80334d5c17422a093f3823
DQ <dqisme@outlook.com>
2022-06-19 15:13:58 +0200

    Add koan for currying

--- koans.ts
+++ koans.ts
@@ -3,3 +3,12 @@ import { assert, assertEquals } from "https://deno.land/std@0.144.0/testing/asse
 Deno.test('true should be true', () => {
     assert(true);
 });
+
+Deno.test('currying', () => {
+    const add = (augend: number, addend: number) => augend + addend;
+    const currying = (func: Function): Function => {
+        // TODO
+    };
+    const curryingAdd = currying(add);
+    assertEquals(curryingAdd(1)(1), 2, 'one plus one equals two');
+});
