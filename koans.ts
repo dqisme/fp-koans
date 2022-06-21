172991cd3b5b8ab7d205fd6cb1fbcdd42e25c6bd
DQ <dqisme@outlook.com>
2022-06-21 14:34:41 +0200

    Implement currying

--- koans.ts
+++ koans.ts
@@ -1,4 +1,4 @@
-import { assert, assertEquals } from "https://deno.land/std@0.144.0/testing/asserts.ts";
+import {assert, assertEquals} from "https://deno.land/std@0.144.0/testing/asserts.ts";
 
 Deno.test('true should be true', () => {
     assert(true);
@@ -6,8 +6,13 @@ Deno.test('true should be true', () => {
 
 Deno.test('currying', () => {
     const add = (augend: number, addend: number) => augend + addend;
-    const currying = (func: Function): Function => {
-        // TODO
+    const currying = (func: Function, ...args: Iterable<unknown>[]): Function => {
+        if (args.length === func.length) {
+            return func(...args);
+        }
+        return (current: Iterable<unknown>) => {
+            return currying(func, ...args, current)
+        }
     };
     const curryingAdd = currying(add);
     assertEquals(curryingAdd(1)(1), 2, 'one plus one equals two');
